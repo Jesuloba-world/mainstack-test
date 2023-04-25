a4ace6fb49e9f489a47f4d6f1b075c0078d8ac1c
Jesuloba <jesulobajohn@gmail.com>
2023-04-25 12:07:40 +0100

    change config to js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	webpack(config: any) {
+	webpack(config) {
 		config.module.rules.push({
 			test: /\.svg$/,
 			use: ["@svgr/webpack"],
